cf28e4089abdcbdf7d210abfdc1d5fdb2890d07e
cesty <vaduva.cezar@yahoo.com>
2026-04-29 15:53:24 +0300

    Export minimal visual UI shell

--- src/ui/__init__.py
+++ src/ui/__init__.py
@@ -9,6 +9,7 @@ from .orchestrator import (
     generate_report_from_ui_request,
     validate_ui_generation_request,
 )
+from .visual import build_request_from_form_values, run_visual_app, submit_visual_form_values
 
 __all__ = [
     "DocxReportGenerator",
@@ -17,7 +18,10 @@ __all__ = [
     "UiGenerationResult",
     "build_parser",
     "build_request_from_args",
+    "build_request_from_form_values",
     "generate_report_from_ui_request",
     "main",
+    "run_visual_app",
+    "submit_visual_form_values",
     "validate_ui_generation_request",
 ]
