4e6f5921c0d046998ab98148d79c2efe0532834c
cesty <vaduva.cezar@yahoo.com>
2026-05-04 13:45:48 +0300

    Test compact audit table renderer

--- tests/test_docx_layout.py
+++ tests/test_docx_layout.py
@@ -4,6 +4,8 @@ from src.report.docx_layout import (
     TOP_ALIGNED_CELL_XML,
     apply_cell_layout_properties,
     apply_table_layout_properties,
+    compact_audit_table,
+    normalize_docx_cell_text,
     table_cell_layout_xml,
     table_layout_properties_xml,
     table_row_properties_xml,
@@ -34,3 +36,30 @@ def test_apply_layout_properties_are_append_only() -> None:
     assert apply_table_layout_properties('<w:tblStyle w:val="TraceAITable"/>').startswith('<w:tblStyle')
     assert '<w:tblLayout w:type="autofit"/>' in apply_table_layout_properties('<w:tblStyle w:val="TraceAITable"/>')
     assert apply_cell_layout_properties('<w:shd w:fill="EDEDED"/>') == '<w:shd w:fill="EDEDED"/><w:vAlign w:val="top"/>'
+
+
+def test_compact_audit_table_contains_print_layout_markers() -> None:
+    xml = compact_audit_table(["Col A", "Col B"], [["value", "other"], [None, ""]])
+
+    assert '<w:tblW w:w="5000" w:type="pct"/>' in xml
+    assert '<w:tblLayout w:type="autofit"/>' in xml
+    assert '<w:tblLook w:firstRow="1"' in xml
+    assert '<w:tblHeader/>' in xml
+    assert '<w:cantSplit/>' in xml
+    assert '<w:vAlign w:val="top"/>' in xml
+    assert '<w:sz w:val="14"/>' in xml
+    assert '<w:sz w:val="13"/>' in xml
+    assert 'FARA DATE IDENTIFICATE' in xml
+
+
+def test_compact_audit_table_escapes_cell_text() -> None:
+    xml = compact_audit_table(["A&B"], [["<lot>"]])
+
+    assert "A&amp;B" in xml
+    assert "&lt;lot&gt;" in xml
+
+
+def test_normalize_docx_cell_text_keeps_missing_explicit() -> None:
+    assert normalize_docx_cell_text(None) == "FARA DATE IDENTIFICATE"
+    assert normalize_docx_cell_text("") == "FARA DATE IDENTIFICATE"
+    assert normalize_docx_cell_text("  abc  ") == "abc"
